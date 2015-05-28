6dfa29ec43e64b0709ed6f2a9f810a1fdbc9558d
zergiocosta <sergio.costa@outlook.com>
2015-05-28 19:40:01 -0300

    testing error msgs loop

--- index.php
+++ index.php
@@ -10,8 +10,21 @@
  * License: GPLv2 or later
  */
 
-function sc_login_error_msg() {
-    return 'Tem algo errado aí';
+/**
+ * Change error msgs
+ */
+function login_error_msg() { 
+    $custom_error_msgs = array(
+        '<strong>EI!</strong>: Tá fazendo o que, aqui?',
+        '<strong>Desista!</strong>: Você não vai entrar aqui!',
+        '<strong>Oxe!</strong>: Acho que você não deveria estar aqui, hein?',
+        '<strong>Opa!</strong>: Calma aê! Sem tentar trapacear!',
+        '<strong>Vish!</strong>: Você não é daqui, né?',
+        '<strong>Deus tá vendo</strong> Você tentando fazer isso!',
+        '<strong>Fuçando</strong> onde não deve, né?',
+        '<strong>Chuck Norris</strong> Não conseguiu logar! Você acha que você vai?',
+        '<strong>YOU</strong> SHALL NOT PASS!',
+    );
+    return $custom_error_msgs[array_rand($custom_error_msgs)];;
 }
-
-add_filter('login_errors', 'sc_login_error_msg');
+add_filter( 'login_errors', 'login_error_msg' );
