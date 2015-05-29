b2c0f8cc09644b8a4bc00170e0c68b99f8ebfdf6
zergiocosta <sergio.costa@outlook.com>
2015-05-29 00:07:07 -0300

    first functional version even for wp repo

--- index.php
+++ index.php
@@ -10,21 +10,15 @@
  * License: GPLv2 or later
  */
 
+require_once plugin_dir_path( __FILE__ ) . '/options-page.php';
+
 /**
  * Change error msgs
  */
 function login_error_msg() { 
-    $custom_error_msgs = array(
-        '<strong>EI!</strong>: Tá fazendo o que, aqui?',
-        '<strong>Desista!</strong>: Você não vai entrar aqui!',
-        '<strong>Oxe!</strong>: Acho que você não deveria estar aqui, hein?',
-        '<strong>Opa!</strong>: Calma aê! Sem tentar trapacear!',
-        '<strong>Vish!</strong>: Você não é daqui, né?',
-        '<strong>Deus tá vendo</strong> Você tentando fazer isso!',
-        '<strong>Fuçando</strong> onde não deve, né?',
-        '<strong>Chuck Norris</strong> Não conseguiu logar! Você acha que você vai?',
-        '<strong>YOU</strong> SHALL NOT PASS!',
-    );
+	$options = get_option( 'sclogin_settings' );
+	$options = explode("\r\n", $options['sclogin_errormsgs']);
+	$custom_error_msgs = $options;
     return $custom_error_msgs[array_rand($custom_error_msgs)];;
 }
 add_filter( 'login_errors', 'login_error_msg' );
