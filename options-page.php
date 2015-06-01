3a99ec0047dfd0d3205284d8973e51e3d623aac6
zergiocosta <sergio.costa@outlook.com>
2015-06-01 19:33:06 -0300

    functions with no params. spaces between ( and )

--- options-page.php
+++ options-page.php
@@ -2,11 +2,11 @@
 add_action( 'admin_menu', 'sclogin_add_admin_menu' );
 add_action( 'admin_init', 'sclogin_settings_init' );
 
-function sclogin_add_admin_menu(  ) { 
+function sclogin_add_admin_menu() { 
 	add_menu_page( 'SC Custom Login', 'SC Custom Login', 'manage_options', 'sc_custom_login', 'sc_custom_login_options_page' );
 }
 
-function sclogin_settings_init(  ) { 
+function sclogin_settings_init() { 
 	register_setting( 'sccustomloginpage', 'sclogin_settings' );
 
 	add_settings_section(
@@ -25,18 +25,18 @@ function sclogin_settings_init(  ) {
 	);
 }
 
-function sclogin_error_msgs_render(  ) { 
+function sclogin_error_msgs_render() { 
 	$options = get_option( 'sclogin_settings' ); ?>
 	<textarea cols='40' rows='5' name='sclogin_settings[sclogin_errormsgs]'> 
 		<?php echo $options['sclogin_errormsgs']; ?>
  	</textarea> <?php
 }
 
-function sclogin_settings_section_callback(  ) { 
+function sclogin_settings_section_callback() { 
 	echo __( 'Customize following the instructions', 'sclogin' );
 }
 
-function sc_custom_login_options_page(  ) { ?>
+function sc_custom_login_options_page() { ?>
 	<form action='options.php' method='post'>
 		<h2>SC Custom Login</h2>
 		<?php
